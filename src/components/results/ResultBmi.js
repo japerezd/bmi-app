f63e12806c2f59c30ee99e8104b6ea75b473303c
Nundu <jorge_alberto1997@hotmail.com>
2020-12-14 22:04:14 -0600

    add require to src in image

--- src/components/results/ResultBmi.js
+++ src/components/results/ResultBmi.js
@@ -54,7 +54,7 @@ export const ResultBmi = () => {
                 <span className="result-bmi">{bmi}</span>
             </div>
 
-            <img src={`../assets/images/${name}`} className="person-weight" alt={name}/>
+            <img src={require(`./assets/images/${name}`)} className="person-weight" alt={name}/>
 
             <div className="range-description">
                 <b>{ range }</b>
